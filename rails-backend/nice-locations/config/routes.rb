f3f259e2913af8e291d2323c4aa993439f2df320
fauli <f@sbebe.ch>
2024-03-12 17:16:44 +0100

    make locations default route

--- rails-backend/nice-locations/config/routes.rb
+++ rails-backend/nice-locations/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "articles#index"
+  root "locations#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/locations", to: "locations#index"
